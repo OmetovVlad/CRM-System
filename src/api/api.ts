7f421592b7eb38aa2a4c0c2a99204ebd8ed82b62
Владислав Ометов <ometov96@mail.ru>
2025-08-17 17:36:59 +0700

    ED-1342: Сделал добавление задач

---
+++ src/api/api.ts
@@ -0,0 +1,17 @@
+export async function createNewTask(title: string) {
+  const response = await fetch('https://easydev.club/api/v1/todos', {
+    method: 'POST',
+    body: JSON.stringify({ title: title }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to create new task');
+  }
+
+  return resData;
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,35 @@
 import { NewTask } from '../../components/NewTask';
+import { createNewTask } from '../../api/api.ts';
+import { useState } from 'react';
+
+interface Task {
+  id: number;
+  title: string;
+  isDone: boolean;
+  created: string;
+}
 
 const Home = () => {
-  const addNewTask = (title: string) => {
-    console.log(title);
+  const initialState: Task[] = [
+    { id: 1, title: 'test', isDone: false, created: '2025-08-17T09:31:56.044667Z' },
+  ];
+
+  const [tasksList, setTasksList] = useState<Task[]>(initialState);
+
+  const addNewTask = async (title: string) => {
+    try {
+      const newTask: Task = await createNewTask(title);
+
+      setTasksList((prev) => [...prev, newTask]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <>
       <h1>Todo List 📋</h1>
       <NewTask handleButton={addNewTask} />
-      <div></div>
     </>
   );
 };
